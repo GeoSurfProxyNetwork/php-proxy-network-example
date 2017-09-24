bfe190dd47899cf03942e9a023314940695edc9e
GeoSurf Residential IPs Proxy Network <32237331+GeoSurfProxyNetwork@users.noreply.github.com>
2017-09-24 14:18:30 +0300

    Update file_get_contents.php

--- file_get_contents.php
+++ file_get_contents.php
@@ -1,6 +1,6 @@
 PHP. file_get_contents
 // PHP file_get_contents proxy example
-// http://www.geosurf.com/resources/residential-ips-integration-guide
+// https://www.geosurf.com/resources/residential-ips-integration-guide
 
 // Our gateway’s hosthname + port, check your dashboard for full gateways list
 $gs_proxy_addr = “gw1.geosurf.io:8081”;
